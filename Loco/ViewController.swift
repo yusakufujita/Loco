e79b581919464ac2bb7a50b4de7bf18e0479b387
藤田優作 <hinasakun@outlook.jp>
2020-03-24 12:17:53 +0900

    Initial Commit

---
+++ Loco/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loco
+//
+//  Created by 藤田優作 on 2020/03/24.
+//  Copyright © 2020 藤田優作. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
